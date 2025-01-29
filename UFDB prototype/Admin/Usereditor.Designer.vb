98c67225296fdc7d1f3fa757a2c08e524562cedc
Bobat3aaa <158512679+Bobat3aaa@users.noreply.github.com>
2025-01-29 18:31:14 +0000

    add user

--- UFDB prototype/Admin/Usereditor.Designer.vb
+++ UFDB prototype/Admin/Usereditor.Designer.vb
@@ -27,7 +27,15 @@ Partial Class Usereditor
         Me.btndelete = New System.Windows.Forms.Button()
         Me.Datagridview = New System.Windows.Forms.DataGridView()
         Me.Lbltitle = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.Txtage = New System.Windows.Forms.TextBox()
+        Me.txtemail = New System.Windows.Forms.TextBox()
+        Me.Checkadmin = New System.Windows.Forms.CheckBox()
         CType(Me.Datagridview, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -51,7 +59,7 @@ Partial Class Usereditor
         '
         'btndelete
         '
-        Me.btndelete.Location = New System.Drawing.Point(303, 150)
+        Me.btndelete.Location = New System.Drawing.Point(288, 150)
         Me.btndelete.Name = "btndelete"
         Me.btndelete.Size = New System.Drawing.Size(205, 46)
         Me.btndelete.TabIndex = 5
@@ -76,21 +84,94 @@ Partial Class Usereditor
         Me.Lbltitle.TabIndex = 9
         Me.Lbltitle.Text = "User editor"
         '
-        'Button1
+        'Label5
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 98)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(205, 46)
-        Me.Button1.TabIndex = 10
-        Me.Button1.Text = "clear"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 96)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(351, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "age:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(345, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "email:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "username:"
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(74, 68)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(178, 20)
+        Me.txtusername.TabIndex = 15
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(74, 94)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.Size = New System.Drawing.Size(178, 20)
+        Me.txtpassword.TabIndex = 14
+        '
+        'Txtage
+        '
+        Me.Txtage.Location = New System.Drawing.Point(385, 68)
+        Me.Txtage.Name = "Txtage"
+        Me.Txtage.Size = New System.Drawing.Size(178, 20)
+        Me.Txtage.TabIndex = 13
+        '
+        'txtemail
+        '
+        Me.txtemail.Location = New System.Drawing.Point(385, 93)
+        Me.txtemail.Name = "txtemail"
+        Me.txtemail.Size = New System.Drawing.Size(178, 20)
+        Me.txtemail.TabIndex = 12
+        '
+        'Checkadmin
+        '
+        Me.Checkadmin.AutoSize = True
+        Me.Checkadmin.Location = New System.Drawing.Point(624, 70)
+        Me.Checkadmin.Name = "Checkadmin"
+        Me.Checkadmin.Size = New System.Drawing.Size(55, 17)
+        Me.Checkadmin.TabIndex = 21
+        Me.Checkadmin.Text = "Admin"
+        Me.Checkadmin.UseVisualStyleBackColor = True
         '
         'Usereditor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(796, 631)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Checkadmin)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.Txtage)
+        Me.Controls.Add(Me.txtemail)
         Me.Controls.Add(Me.Lbltitle)
         Me.Controls.Add(Me.Datagridview)
         Me.Controls.Add(Me.btnadd)
@@ -109,5 +190,13 @@ Partial Class Usereditor
     Friend WithEvents btndelete As Button
     Friend WithEvents Datagridview As DataGridView
     Friend WithEvents Lbltitle As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtusername As TextBox
+    Friend WithEvents txtpassword As TextBox
+    Friend WithEvents Txtage As TextBox
+    Friend WithEvents txtemail As TextBox
+    Friend WithEvents Checkadmin As CheckBox
 End Class
--- UFDB prototype/Admin/Usereditor.vb
+++ UFDB prototype/Admin/Usereditor.vb
@@ -1,4 +1,6 @@
-﻿Public Class Usereditor
+﻿Imports System.Text.RegularExpressions
+
+Public Class Usereditor
 
     Private userlist As List(Of User)
     Private ranklist As List(Of ranking)
@@ -24,20 +26,26 @@
     End Sub
 
     Private Sub btnadd_Click(sender As Object, e As EventArgs) Handles btnadd.Click
-
-        Dim lastRow As Integer = Datagridview.Rows.Count - 1
-
-        If lastRow >= 0 And Datagridview.CurrentRow.Cells(0).Value Is Nothing Then
-            Datagridview.CurrentCell = Datagridview.Rows(lastRow).Cells(0)
-            Datagridview.FirstDisplayedScrollingRowIndex = lastRow
+        Dim newusername As String = txtusername.Text
+        Dim newpassword As String = txtpassword.Text
+        Dim newage As Integer = Val(Txtage.Text)
+        Dim newemail As String = txtemail.Text
+        Dim admindecision As Boolean = Checkadmin.Checked
+        Debug.WriteLine(admindecision)
+        If validatepassword(newpassword) = True Then
+            If validateemail(newemail) = True Then
+                Adduser(newusername, newpassword, newage, newemail, admindecision)
+            Else
+                MsgBox("Email not valid")
+            End If
         Else
+            MsgBox("Email must have 8-32 characters, one special character and a capital letter")
 
-
-            Datagridview.CurrentCell = Datagridview.Rows(lastRow).Cells(0)
-            Datagridview.FirstDisplayedScrollingRowIndex = lastRow
         End If
-        Dim nextuserid As Integer = GetNextUserID(userlist)
-        Datagridview.CurrentRow.Cells(0).Value = nextuserid
+
+
+        updatedatabase()
+
     End Sub
     Function GetNextUserID(users As List(Of User)) As Integer
         If users.Count = 0 Then
@@ -62,4 +70,76 @@
         functions.SaveTolikedfighterJson(likedfighterlist)
         functions.SaveToRanklistJson(ranklist)
     End Sub
+
+    Private Sub Adduser(newusername, newpassword, newage, newemail, admin)
+
+        'gets encrypted password
+
+        Dim newuser As New User()
+        newuser.passwordlength = Len(newpassword)
+        Dim encryptpass As String = encryptpassword(newusername, newpassword)
+
+
+
+        newuser.username = newusername
+        newuser.age = newage
+        newuser.password = encryptpass
+        newuser.email = newemail
+        newuser.Admin = admin
+        'make list of existing users
+
+        newuser.UserID = GetNextUserID(userlist)
+        If ValidateUser(newuser) Then
+            userlist.Add(newuser)
+
+            MsgBox("New user added!")
+        Else
+            MsgBox("User not added.")
+        End If
+    End Sub
+    Private Function validatepassword(ByVal password As String)
+        Static passwordcheck As New Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$")
+        MsgBox(passwordcheck.IsMatch(password))
+        Return passwordcheck.IsMatch(password)
+    End Function
+
+    Function encryptpassword(newusername, newpassword)
+
+        Dim encryptpass As String
+        Dim key As Integer
+        Dim addedchar As Char
+        Dim currentchar As Integer = 0
+
+        'makes key for password using username
+        key = (Asc(newusername(0)) + (Asc(newusername(newusername.Length - 1))))
+
+        'adds letters to the password
+        While newpassword.Length < 32
+            addedchar = Chr(Int((Asc((newpassword(currentchar))) + Asc((newpassword(currentchar)))) / 3))
+            currentchar += 1
+            newpassword += addedchar
+        End While
+
+        'encrypts password with xor key
+        For i As Integer = 0 To newpassword.Length - 1
+            encryptpass &= Chr(Asc((newpassword(i))) Xor key)
+        Next
+
+
+        Return encryptpass
+    End Function
+    Function ValidateUser(user As User) As Boolean
+        If String.IsNullOrEmpty(user.username) OrElse String.IsNullOrEmpty(user.password) OrElse user.age <= 0 OrElse Not user.email.Contains("@") Then
+            Return False
+        End If
+        Return True
+
+
+
+    End Function
+    Function validateemail(ByVal email As String) As Boolean
+        'regular expression to check if email is in correct format
+        Static emailExpression As New Regex("^[_a-z0-9-]+(.[a-z0-9-]+)@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$")
+        Return emailExpression.IsMatch(email)
+    End Function
 End Class
